eb3e97b5a90aca2dc38e015e86c3583256400456
Pierre-Louis789 <sicot.pierrelouis@gmail.com>
2026-03-26 10:58:59 +0000

    Add basic home and restaurant views

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Restaurant
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def restaurant_detail(request):
+    restaurant = Restaurant.objects.first()
+    return render(request, 'restaurant_detail.html', {'restaurant': restaurant})
