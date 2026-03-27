b52a5a978c6fb7010a66e278dc9d8c64fbabb662
Pierre-Louis789 <sicot.pierrelouis@gmail.com>
2026-03-27 12:31:19 +0000

    Import Booking model in my_bookings view

--- bookings/views.py
+++ bookings/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Restaurant
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from .models import Booking
 from .forms import BookingForm
 from django.contrib.auth.decorators import login_required
 
