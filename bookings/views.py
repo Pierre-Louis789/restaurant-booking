9860caf79b7153023e081522a4b5b413a2071235
Pierre-Louis789 <sicot.pierrelouis@gmail.com>
2026-03-27 10:54:29 +0000

    Add create_booking view with form handling and success message

--- bookings/views.py
+++ bookings/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import Restaurant
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import BookingForm
+
 
 def home(request):
     return render(request, 'home.html')
@@ -7,3 +11,18 @@ def home(request):
 def restaurant_detail(request):
     restaurant = Restaurant.objects.first()
     return render(request, 'restaurant_detail.html', {'restaurant': restaurant})
+
+def create_booking(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+        booking = form.save(commit=False)
+        booking.user = request.user
+        booking.save()
+        messages.success(request, "Your booking has been created!")
+        return redirect('home')
+    
+    else:
+        form = BookingForm()
+
+    return render(request, 'create_booking.html', {'form': form})
