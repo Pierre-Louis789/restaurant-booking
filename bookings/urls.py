991e381d283ca76dff187145d14235a7460149d2
Pierre-Louis789 <sicot.pierrelouis@gmail.com>
2026-03-27 10:50:34 +0000

    Add URL route for creating bookings

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('restaurant/', views.restaurant_detail, name='restaurant_detail'),
+    path('book/', views.create_booking, name='create_booking'),
 ]
