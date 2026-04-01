c8956102676027c5b26cced3416ddeb7e89083a5
Pierre-Louis789 <sicot.pierrelouis@gmail.com>
2026-04-01 21:00:58 +0100

    add whitenoise and path

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -121,10 +121,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-    ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+    os.path.join(BASE_DIR, 'static'),
+]
+
+
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
+
 
 
 # MEDIA SETTINGS
