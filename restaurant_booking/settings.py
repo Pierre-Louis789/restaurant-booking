a57940d82bbed04995a69536ec5a46bd2b95e60d
Pierre-Louis789 <sicot.pierrelouis@gmail.com>
2026-06-15 14:55:55 +0100

    Fix template path for base.html

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG", "False") == "True"
+
 
 ALLOWED_HOSTS = ["*", "localhost", "127.0.0.1", "restaurant-booking-qcpf.onrender.com"]
 
