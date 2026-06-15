85aeffc9b8ddaac637c02b752f7f812025d42dfb
Pierre-Louis789 <sicot.pierrelouis@gmail.com>
2026-06-15 18:09:11 +0100

    delete seed url

--- bookings/urls.py
+++ bookings/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', views.custom_logout, name='logout'),
-    path("quick-seed/", views.quick_seed, name="quick_seed"),
+    
 
 ]
