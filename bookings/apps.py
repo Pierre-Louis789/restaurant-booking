5fa9b81334943e69488f2a877ff36c529d0d62bb
Pierre-Louis789 <sicot.pierrelouis@gmail.com>
2026-06-15 17:18:08 +0100

    fixing app

--- bookings/apps.py
+++ bookings/apps.py
@@ -6,28 +6,4 @@ class BookingsConfig(AppConfig):
     name = 'bookings'
 
     def ready(self):
-        from bookings.models import Restaurant
-        from datetime import time
-
-        if Restaurant.objects.count() == 0:
-            Restaurant.objects.create(
-                name="Bistro West",
-                description="A cosy modern bistro offering seasonal dishes.",
-                address="14 West Street, Penzance",
-                opening_time=time(11, 0),
-                closing_time=time(22, 0),
-                cuisine="Modern European",
-                static_image="restaurant_images/bistro-west.jpg"
-            )
-
-            Restaurant.objects.create(
-                name="Sakura Sushi House",
-                description="Fresh sushi, sashimi, ramen, and Japanese dishes.",
-                address="7 Cherry Blossom Lane, Penzance",
-                opening_time=time(12, 0),
-                closing_time=time(22, 30),
-                cuisine="Japanese",
-                static_image="restaurant_images/sakura.jpg"
-            )
-
-            print("🔥 Restaurants seeded on startup")
+        pass
